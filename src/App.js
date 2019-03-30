c3bd9f5a3df601e10b0c6fe6c5e875b6aa20e856
rudyyazdi <rudyyazdi@gmail.com>
2019-03-30 23:44:35 +1100

    make app a functional component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import './app.css';
 import Nav from './Nav';
 import SearchInput from './SearchInput'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Nav />
-        <div className="container">
-          <SearchInput className="main-search-input" />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Nav />
+    <div className="container">
+      <SearchInput className="main-search-input" />
+    </div>
+  </div>
+)
 
 export default App;
