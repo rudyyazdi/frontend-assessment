a514b4f7b46a294bc95110207eed7bf9571247f6
rudyyazdi <rudyyazdi@gmail.com>
2019-03-31 00:22:53 +1100

    sorted the spinner

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import Loader from 'react-loader-spinner'
 
 import './search-input.css'
 
 const SearchInput = ({ className }) => (
   <div className={classNames([className, 'search-input'])}>
     <input type="text" placeholder="Search.." />
+    <div className="loader-spinner">
+      <Loader
+        type="Oval"
+        color="grey"
+        height="30"
+        width="30"
+      />
+    </div>
   </div>
 )
 
