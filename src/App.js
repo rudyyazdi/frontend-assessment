e01ab66f3dc7807d398a3c8b85152bf2987e9bfe
rudyyazdi <rudyyazdi@gmail.com>
2019-03-30 21:53:52 +1100

    added the nav (mobile)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import Nav from './Nav';
+
 class App extends Component {
   render() {
     return (
       <div>
+        <Nav />
         hello
       </div>
     );
---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './nav.css'
+
+const Nav = () => (
+  <nav>
+    <div className="container">
+      <div>
+        <label className="menu">
+          <span></span>
+        </label>
+      </div>
+      <div>
+        <h1 className="logo">Marvel</h1>
+      </div>
+      <div>
+        <input type="text" className="search-input" placeholder="Search" />
+      </div>
+    </div>
+  </nav >
+)
+
+export default Nav
